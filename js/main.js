a8b4328d3a5befe95573f3d071077bc40ea3cd0d
aleksandra <ola88mazurek@gmail.com>
2021-02-24 17:54:47 +0100

    kolejny projekt js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let firstNumber = 79;
+console.log(firstNumber);
+
+let secondNumber = 42;
+console.log(secondNumber);
+
+let num1 = firstNumber;
+console.log(num1);
+
+let num2 = secondNumber;
+console.log(num2);
+
+function sum(num1, num2) {
+let result = num1 + num2;
+
+return result;
+}
